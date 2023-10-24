aa5a5a06d32dcca6d2fb70166de6d0da02e4d53a
shivam singh rajput <148266278+shivam-1205@users.noreply.github.com>
2023-10-24 20:44:22 +0000

    conversion of datatypes

---
+++ shivam/conversion_opration.js
@@ -0,0 +1,25 @@
+/*
+let score="33";
+console.log(typeof(score));
+console.log(typeof score);
+
+let valueofsting=Number(score);
+console.log(typeof valueofsting);  //number convert 
+console.log(valueofsting);//nan not a number
+//number convert "33"=>33
+//"33abe" =>not a number Nan
+// true=>1; false=>0
+*/
+/*let isloggedIn="shivam";
+let booleanIsLockedIn=Boolean(isloggedIn);
+console.log(booleanIsLockedIn);
+//boolean
+//1=>true: 0=>false
+//""=false
+//"shivam"=>true
+*/
+let someNumber=33
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
