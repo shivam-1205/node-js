832a965436f7d34b37361f8dbadb2dc10e5808f3
shivam singh rajput <148266278+shivam-1205@users.noreply.github.com>
2023-10-24 20:17:39 +0000

    datatypes

---
+++ shivam/Datatypes.js
@@ -0,0 +1,24 @@
+"use strict"//treat all js cose newer version
+
+//alert(3+3);//we are using node.js not browser
+
+console.log(3+3);
+console.log(4+1);
+
+let name="shivam"
+let age=22
+let isloggedin=false
+
+//Datatypes
+//number=>2to power of 53
+//bigint=> big number
+//string=>"--"
+//boolean=> true,false
+//null=> empty value, standalone value
+//undfined=> value not declear,not put a value
+//symbol=>unique 
+
+
+//object
+
+console.log(typeof null);//object
