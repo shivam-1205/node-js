28c907b8b4802f9d529d5e268c6f4eeaadedf06b
shivam singh rajput <148266278+shivam-1205@users.noreply.github.com>
2023-10-24 20:17:53 +0000

    var

---
+++ shivam/var.js
@@ -0,0 +1,20 @@
+const accountId=144553;
+let accountEmail="shivamsingh@gmail.com";
+var accountpassword="12345"
+accountcity="nashik";
+
+let accountstate;//undeined
+accountpassword="1254"
+accountEmail="skja@1.com"
+accountcity="sinnar"
+
+//accountId=2//not allowed
+console.log(accountId);
+/*
+prefer not use var 
+because of issue in block scope and function scope
+*/
+
+console.table([accountId,accountEmail,accountcity,accountpassword,accountstate]);//it print in the box row/table
+console.log(accountEmail,accountId);//it print in line
+
