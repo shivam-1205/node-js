39ae0c3fb9eaac44b4b3d748eb7145b84bfcbc36
shivam singh rajput <148266278+shivam-1205@users.noreply.github.com>
2023-10-27 21:58:44 +0000

    Number and math

--- basic01/num_And_math.js
+++ basic01/num_And_math.js
@@ -6,17 +6,18 @@ const balance=new Number(100)
 
 console.log(balance.toString());
 
-// console.log(balance.toFixed(2));//add two zero
-// const othernumber=2314.456;//23456.45 then the value exponential notation might be returned in some circumstances
-// console.log( othernumber.toPrecision(3)); 
+// console.log(balance.toFixed(2));//add two zero 100.00
 
-// const hundreds=100000
-// console.log(hundreds.toLocaleString('en-IN'));----indian number main , lagega
+//const othernumber=2314.456;//23456.45 then the value exponential notation might be returned in some circumstances
+ //console.log( othernumber.toPrecision(3)); 
+
+// const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'));----indian number main , lagega 10,00,000
 
 //************Math*************** */
 // console.log(Math);//object
 // console.log(Math.abs(-5));//convert to negative to positive
-// console.log(Math.round(3.4));//top value op-3
+// console.log(Math.round(3.4));//top value op-3 round fig
 // console.log(Math.ceil(4.3)); //uppervalue
 // console.log(Math.floor(2.9));//lower value
 
@@ -24,8 +25,8 @@ console.log(balance.toString());
 
 //console.log(Math.floor(Math.random()*10)+1)
 
-// const min=10
-// const max=20
+ const min=10
+ const max=20
 
-// console.log(Math.floor((Math.random()*(max-min +1)))+min)//imp hai likh lo
+ console.log(Math.floor((Math.random()*(max-min +1)))+min)//imp hai likh lo
 
