ee307bb87d754a85ea54479eb50b2e9d2d33700b
shivam singh rajput <148266278+shivam-1205@users.noreply.github.com>
2023-10-27 18:57:09 +0000

    math and num

---
+++ shivam/num_And_math.js
@@ -0,0 +1,31 @@
+// const score=400;
+// console.log(score);
+
+// const balance=new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));//add two zero
+// const othernumber=2314.456;//23456.45 then the value exponential notation might be returned in some circumstances
+// console.log( othernumber.toPrecision(3)); 
+
+// const hundreds=100000
+// console.log(hundreds.toLocaleString('en-IN'));----indian number main , lagega
+
+//************Math*************** */
+// console.log(Math);//object
+// console.log(Math.abs(-5));//convert to negative to positive
+// console.log(Math.round(3.4));//top value op-3
+// console.log(Math.ceil(4.3)); //uppervalue
+// console.log(Math.floor(2.9));//lower value
+
+//console.log(Math.random())//always random value come
+
+//console.log(Math.floor(Math.random()*10)+1)
+
+const min=10
+const max=20
+
+console.log(Math.floor((Math.random()*(max-min +1)))+min)//imp hai likh lo
+
