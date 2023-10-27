c4e25855d8104a76dc4a96e4a197ea02294d740b
shivam singh rajput <148266278+shivam-1205@users.noreply.github.com>
2023-10-27 20:04:47 +0000

    string

---
+++ basic01/string.js
@@ -0,0 +1,41 @@
+// const name="shivam"  //string declare 
+// const age=23;
+
+// console.log(name,age+"value");
+// console.log(`i am ${name} singh, my age is ${age} year old`);
+
+//we use string prototypes
+//const gamename=new String('shivam')
+ //console.log(gamename[0]);
+//console.log(gamename.__proto__);
+
+
+// console.log(gamename.length);----for length
+
+
+// console.log(gamename.toUpperCase())----for upper case
+
+ //const newString=gamename.substring(1,5);//----for 1to5 ko haata dega
+ //console.log(newString);
+
+
+//**********we use -at- _prtotypes_********* */
+// const string =[20,30,50,56,65];
+// let index=-2;
+// index=2;
+// index=-0
+// console.log(`let print ${index}, the item return is ${string.at(index)}`);
+
+
+// concet prototypes
+//  array1=[10,20,30,40,50]
+//  array2=[60,70,80,90,100]
+//  array3=array1.concat(array2);
+// console.log(array3);
+
+
+//copywithin
+//array1=[10,20,30,40,50];
+//1console.log(array1.copyWithin(0,2,3));
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every ---link for all string
