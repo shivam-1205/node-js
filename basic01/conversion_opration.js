cb2b72217314bb08a0a8af2afa0f36b1146b2be9
shivam singh rajput <148266278+shivam-1205@users.noreply.github.com>
2023-10-27 20:03:23 +0000

    conversion and oprations

---
+++ basic01/conversion_opration.js
@@ -0,0 +1,60 @@
+/*
+let score="33";
+console.log(typeof(score));
+console.log(typeof score);
+
+let valueofsting=Number(score);
+console.log(typeof valueofsting);  //number convert 
+console.log(valueofsting);//nan not a number
+//number convert "33"=>33
+//"33abe" =>not a number Nan
+// true=>1; false=>0
+*/
+/*let isloggedIn="shivam";
+let booleanIsLockedIn=Boolean(isloggedIn);
+console.log(booleanIsLockedIn);
+//boolean
+//1=>true: 0=>false
+//""=false
+//"shivam"=>true
+*/
+/*let someNumber=33
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);11*/
+
+//**************oprations**************** */
+let value=3
+let negValue=-value
+//console.log(negValue); the value is -3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%3);
+
+// let num1= "shivam"
+// let num2=1205
+// let num3=num1+num2
+
+// console.log(num3);
+// console.log(2+"2");
+// console.log(3+2+"3");
+// console.log("3"+2+3);
+// console.log("2"+2);//rule ecma to primitive
+
+//increment(++)
+let postfix=2;
+postfix++;
+console.log(postfix);
+
+let prefix=2;
+++prefix;
+console.log(prefix);
+
+var cal=10
+var cal=20
+console.log(cal);
+
