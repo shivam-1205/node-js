8cc4c58d5860732872819ed5b5e7164a36f4e0e6
shivam singh rajput <148266278+shivam-1205@users.noreply.github.com>
2023-11-01 20:55:45 +0000

    if and else

---
+++ control_Flow_04/one.js
@@ -0,0 +1,88 @@
+//Statements if and else //condition 
+
+// if (true) {
+    
+// }
+// if (false) {
+    
+// }
+
+//< ,> <=, >=,==,===,!=,!==,??,||,&&
+
+/*
+const temp=45;
+if (temp<40) {
+     console.log("high temp");
+}
+else{
+    console.log("low temp");
+};
+*/
+
+/*
+const num =40
+if (num>30) {
+    console.log("yes");
+}
+else{
+   console.log("no");
+};
+*/
+
+/*
+const temp =41;//temp equal hai kya 41 ka
+if (temp==50) {
+    console.log("temp is grater then 50"); 
+}
+  console.log("temp is less the 50");//both print
+
+*/
+//kya 3 ,2 ke baber nahi hai
+/*if (3!=2) {
+     console.log("yes");//righ
+ }
+else{
+     console.log("no");
+}
+    */ 
+
+// if (4===4) {
+//     console.log("yes");
+    
+// }
+// console.log("no")
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+// const userLoggedIn = true
+// const debitCard = true
+// const loggedInFromGoogle = false
+// const loggedInFromEmail = true;
+
+// if (userLoggedIn && debitCard &&  2<4  ) {// "And" always get true----- left and right true
+//     console.log("Allow to buy course");
+// };
+
+// if (loggedInFromGoogle || loggedInFromEmail) {// "||= OR" multipal condision check ----unmain se true hina chahaiya
+//     console.log("User logged in");
+// }
+
+
+
+
