714fa91f522d338ebb7aed0e35d38ccbb2683c83
shivam singh rajput <148266278+shivam-1205@users.noreply.github.com>
2023-11-01 20:53:27 +0000

    for loop

---
+++ loop_05/for.js
@@ -0,0 +1,50 @@
+//for loop
+for (let i = 0; i <10; i++) {
+    const element =i;
+    if (element==5) {
+       // console.log("5 is a best number");
+    }
+   //console.log(element);
+}
+
+// for (let i = 0; i < 3; i++) {
+    
+//    for (let j = 0; j < 3; j++) {
+//   // console.log(`inner loop value:${j}`);
+//   console.log(i+ '*'+ j +' = '+ i*j);//table
+//    }
+//    console.log(`outer loop value:${i}`);
+// }
+
+////////////////////diffrent betwwen 
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(`outer loop value:${i}`);
+//     for (let j = 0; j < 3; j++) {
+//    // console.log(`inner loop value:${j}`);
+//    console.log(i+ '*'+ j +' = '+ i*j);//table
+//     }
+    
+//  }
+
+// let myarray=["superman","batman","flash","aquaman"]
+// console.log(myarray.length);//4
+// for (let index = 0; index < myarray.length; index++) {
+//     const element = myarray[index];
+//    // console.log(index);//print 0,1,2,3
+//    // console.log(myarray);//print all array same
+//     console.log(element);//print superman,btman,flash,aquaman
+    
+// }
+
+//break and continue
+
+for (let index = 1; index <=20; index++) {
+    if (index==5) {
+        console.log('detected 5');
+       // break
+        continue
+    }
+  console.log(`value of i is ${index}`);
+    
+}
