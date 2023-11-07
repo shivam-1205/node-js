066120ed97caf6b212fc1ec7449e3d1f27f42c4f
shivam singh rajput <148266278+shivam-1205@users.noreply.github.com>
2023-11-07 19:46:42 +0000

    for in 04

---
+++ loop_05/four.js
@@ -0,0 +1,18 @@
+//for_in loop //specific use for object
+// loops through the properties of an object
+
+// for (const key in object) {
+//     
+// }
+
+const myobject={
+    'game':'NFS',
+    'game2':'spiderman',
+    'game3':'pubg'
+}
+for (const key in myobject) {
+   // console.log(key);
+//   console.log(myobject[key]);
+//console.log(`${key} shortcut is for ${myobject[key]}`);
+
+}
