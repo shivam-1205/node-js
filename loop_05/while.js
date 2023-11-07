527a8d9c451cfd0ab7fb486e184710569edc52e2
shivam singh rajput <148266278+shivam-1205@users.noreply.github.com>
2023-11-07 19:48:59 +0000

    while loop 0

---
+++ loop_05/while.js
@@ -0,0 +1,24 @@
+//while loop //he while statement creates a loop that executes a specified statement as long as the test condition evaluates to true.
+// joh while loop hota hai executes karta hai specified statement jab tak condition true nahi hota tab tak execute hota hai
+
+let index=0 
+while (index <=10) {
+   // console.log(`value of index is ${index}`);
+    index=index+2
+};
+
+let myarray=["hello","hey","hii","yo"];
+let arr=0
+while (arr < myarray.length) {
+   // console.log(`value is ${myarray[arr]}`);
+    arr=arr+1
+};
+
+// do while loop  // creates a loop that executes a specified statement until the test condition evaluates to false.
+//joh do while loop hota hai executes karta hai specified statement jab tak condition false nahi hota tab tak execute hota hai
+
+let score=11;
+do {
+    console.log(`score is ${score}`);
+    score++
+} while ( score<=10);
